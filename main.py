26f22368d75fcf223a891f79a308b2b1a97f3320
childstrangeler <jesperbogoemoe@gmail.com>
2023-03-20 15:12:23 +0100

    billet.py tilføjet

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=50)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=1, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=1)
-ttk.Button(frm, text="farve",).grid(column=0, row=1)
-root.mainloop()
+# from tkinter import *
+# from tkinter import ttk
+# root = Tk()
+# frm = ttk.Frame(root, padding=50)
+# frm.grid()
+# ttk.Label(frm, text="Hello World!").grid(column=1, row=0)
+# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=1)
+# ttk.Button(frm, text="farve",).grid(column=0, row=1)
+# root.mainloop()
